36813297a0bb976dfa2863047e24cf76fe811649
Bala-s-dev <baladev2004@gmail.com>
2026-04-27 20:01:39 +0530

    pattern code

---
+++ patterns/pyramid.java
@@ -0,0 +1,24 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            //for space
+            for(int space = 1; space <= n - i; space++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= (2 * i - 1); j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
