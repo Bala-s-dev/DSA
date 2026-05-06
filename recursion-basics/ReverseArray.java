fe6a21279a8576199e519291310f6e4fdf62e95e
Bala-s-dev <baladev2004@gmail.com>
2026-05-06 10:06:54 +0530

    Reverse a array using recursion'

---
+++ recursion-basics/ReverseArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class ReverseArray {
+
+    public static void reversearr(int[] arr, int start, int end){
+        if(start >= end){
+            return;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        reversearr(arr, start + 1, end - 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        reversearr(arr, 0, n - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }   
+}
